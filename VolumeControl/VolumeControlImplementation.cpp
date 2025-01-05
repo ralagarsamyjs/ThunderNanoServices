b2fb3e22998e0d3cb8c3a1dc4d5ef92ca4e99a62
ralagarsamyjs <ralagarsamy2020@gmail.com>
2025-01-05 12:53:38 +0530

    added debug message

--- VolumeControl/VolumeControlImplementation.cpp
+++ VolumeControl/VolumeControlImplementation.cpp
@@ -32,6 +32,7 @@ namespace Plugin {
                                 [this]() { NotifyVolumeChange(); },
                                 [this]() { NotifyMutedChange(); }))}
     {
+        printf("WPEFramework::Plugin::VolumeControlImplementation()->PID<%d><%d> Conxtructor\n", getpid(), gettid());
     }
 
     VolumeControlImplementation::~VolumeControlImplementation() = default;
@@ -40,7 +41,9 @@ namespace Plugin {
     {
         ASSERT(notification);
         _adminLock.Lock();
+        printf("WPEFramework::Plugin::VolumeControlImplementation::Register()->PID<%d><%d> notification->AddRef()\n", getpid(), gettid());
         notification->AddRef();
+        printf("WPEFramework::Plugin::VolumeControlImplementation::Register()->PID<%d><%d> _notifications.push_back(notification)\n", getpid(), gettid());
         _notifications.push_back(notification);
         _adminLock.Unlock();
     }
@@ -49,9 +52,11 @@ namespace Plugin {
     {
         ASSERT(notification);
         _adminLock.Lock();
+        printf("WPEFramework::Plugin::VolumeControlImplementation::Unregister()->PID<%d><%d>_notifications.erase(item)\n", getpid(), gettid());
         auto item = std::find(_notifications.begin(), _notifications.end(), notification);
         ASSERT(item != _notifications.end());
         _notifications.erase(item);
+        printf("WPEFramework::Plugin::VolumeControlImplementation::Unregister()->PID<%d><%d>(*item)->Release()\n", getpid(), gettid());
         (*item)->Release();
         _adminLock.Unlock();
     }
@@ -60,11 +65,13 @@ namespace Plugin {
     uint32_t VolumeControlImplementation::Muted(const bool muted)
     {
         TRACE(Trace::Information, (_T("Set Muted: %s"), muted ? _T("true") : _T("false")));
+        printf("WPEFramework::Plugin::VolumeControlImplementation::Muted(muted)->PID<%d><%d>_platform->Muted(muted)\n", getpid(), gettid());
         return _platform->Muted(muted);
     }
 
     uint32_t VolumeControlImplementation::Muted(bool& muted) const
     {
+        printf("WPEFramework::Plugin::VolumeControlImplementation::Muted(muted)->PID<%d><%d>_platform->Muted(muted)\n", getpid(), gettid());
         muted = _platform->Muted();
         TRACE(Trace::Information, (_T("Get Muted: %s"), muted ? _T("true") : _T("false")));
         return Core::ERROR_NONE;
@@ -73,11 +80,13 @@ namespace Plugin {
     uint32_t VolumeControlImplementation::Volume(const uint8_t volume)
     {
         TRACE(Trace::Information, (_T("Set Volume: %d"), volume));
+        printf("WPEFramework::Plugin::VolumeControlImplementation::Volume(vol)->PID<%d><%d>_platform->Volume(volume)\n", getpid(), gettid());
         return _platform->Volume(volume);
     }
 
     uint32_t VolumeControlImplementation::Volume(uint8_t& vol) const
     {
+        printf("WPEFramework::Plugin::VolumeControlImplementation::Volume(&vol)->PID<%d><%d>_platform->Volume()\n", getpid(), gettid());
         vol = _platform->Volume();
         TRACE(Trace::Information, (_T("Get Volume: %d"), vol));
         return Core::ERROR_NONE;
@@ -86,6 +95,7 @@ namespace Plugin {
     void VolumeControlImplementation::NotifyVolumeChange()
     {
         _adminLock.Lock();
+        printf("WPEFramework::Plugin::VolumeControlImplementation::NotifyVolumeChange()->PID<%d><%d>notification->Volume(_platform->Volume())\n", getpid(), gettid());
         for (auto* notification : _notifications) {
             notification->Volume(_platform->Volume());
         }
@@ -95,6 +105,7 @@ namespace Plugin {
     void VolumeControlImplementation::NotifyMutedChange()
     {
         _adminLock.Lock();
+        printf("WPEFramework::Plugin::VolumeControlImplementation::NotifyMutedChange()->PID<%d><%d>notification->Muted(_platform->Muted())\n", getpid(), gettid());
         for (auto* notification : _notifications) {
             notification->Muted(_platform->Muted());
         }
